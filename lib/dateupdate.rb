56f838613267c3ede4f6c8995bb67e9091da22ca
Nic Nilov <nicnilov@gmail.com>
2015-01-22 20:42:42 +0300

    control script streamlined

--- lib/dateupdate.rb
+++ lib/dateupdate.rb
@@ -1,28 +1,35 @@
 require 'highline/import'
-require_relative 'dateupdate/logging'
 require_relative 'dateupdate/auth'
 
 module DateUpdate
+
+  def self.execute
+    display_welcome
+    authenticate
+  end
+
   def self.display_welcome
     say ("===========================================================\n"\
          "            Flickr date taken fixing script                \n"\
          '===========================================================')
   end
 
-  def self.start
-    display_welcome
+  def self.authenticate
     consumer_key = ask('Enter consumer key: ')
     consumer_secret = ask('Enter consumer secret: ')
     auth = DateUpdate::Auth.new(consumer_key, consumer_secret)
     auth.request_token
-    true
+    say("\nGo by this url to authorize this script to access your Flickr account:\n")
+    say(auth.user_authorization_url + "\n")
+    verifier = ask('Enter the code given by Flickr: ')
+    auth.access_token(verifier)
+    say("\nSuccessfully authenticated\n")
   end
 end
 
 begin
-  DateUpdate.start ? ask('Finished successfully') : ask('Error')
+  DateUpdate.execute
+  say('Finished successfully')
 rescue => error
-  include Logging
-  logger.error("Exception: #{error.message}\n\n#{error.backtrace}")
-  ask('Error')
+  say("Exception: #{error.message}\n\n#{error.backtrace}")
 end
